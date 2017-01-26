90aefb9bad9ee1202931ee15d6e3ad8006f31361
Pete Corey <petecorey@gmail.com>
2017-01-26 18:40:29 -0500

    Working!

--- lib/life/supervisor.ex
+++ lib/life/supervisor.ex
@@ -2,11 +2,14 @@ defmodule Life.Supervisor do
   use Supervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, [])
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init(_) do
-    children = [worker(Life.Universe, [])]
+    children = [
+      worker(Life.Universe, []),
+      supervisor(Life.CellSupervisor, [])
+    ]
     supervise(children, strategy: :one_for_one)
   end
 
